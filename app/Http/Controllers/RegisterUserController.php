d156cd940f4477137650b0b315504e7000f39f5a
minsuwai <mswwai15@gmail.com>
2024-11-05 00:19:41 +0630

    feat: create register controller and form

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterUserController extends Controller
 {
@@ -27,7 +31,29 @@ class RegisterUserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $userAttributes = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'confirmed', Password::min(6)],
+        ]);
+
+        $employerAttributes = $request->validate([
+            'employer' => ['required'],
+            'logo' => ['required', File::types(['png', 'jpg', 'webp'])],
+        ]);
+
+        $user = User::create($userAttributes);
+
+        $logoPath = $request->logo->store('logos');
+
+        $user->employer()->create([
+            'name' => $employerAttributes['employer'],
+            'logo' => $logoPath,
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/');
     }
 
     /**
